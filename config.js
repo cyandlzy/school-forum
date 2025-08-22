1ef0b3a7a366767a4512091e10f1cd936e9a9e8f
cyandlzy <hangmuzhuanyon@outlook.com>
2025-08-22 21:29:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const supabaseUrl = 'YOUR_SUPABASE_URL'; // 替换为你的 Project URL
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY'; // 替换为你的 anon public Key
+const supabaseUrl = 'https://ntqzspumxbjarombbftg.supabase.co'; // 替换为你的 Project URL
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50cXpzcHVteGJqYXJvbWJiZnRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU4NjY3MjMsImV4cCI6MjA3MTQ0MjcyM30.eyHi1511JGHFrzyeJQ6DjdM_LYIDE_vCmtOjgtMdoq4'; // 替换为你的 anon public Key
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
